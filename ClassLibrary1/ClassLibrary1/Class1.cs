80548ec69ab79f091920e50848404f65bbd0df44
Tom Soyer <tomsoyer@mail.ru>
2020-02-13 16:32:39 +0500

    второй прототип

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+ 
+namespace VIN_LIB
+{
+    public class Program
+    {
+        public bool CheckVIN (string[] vin)
+        {
+            string s = "vvvv";
+            Regex regex = new Regex(@"\d{3}-\d{3}-\d{4}");
+            if (regex != )
+            {
+                Console.WriteLine("Истина");
+            }
+            else
+            {
+                Console.WriteLine("Ложь");
+            }
+        }
+
+        
+    }
+}
--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(511, 125);
+            this.button1.Location = new System.Drawing.Point(353, 238);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
